e03bf07f516154b0edf9713dfc76f7541b9d7be4
Philip Marais <Philip.Marais@Multiply.co.za>
2021-09-14 01:45:02 +0200

    Code comments and fixing spaces/tabs

--- click2sure_bank/main/helpers.py
+++ click2sure_bank/main/helpers.py
@@ -1,22 +1,39 @@
+## Import the required models iobjects
 from main.models import *
 
+## Select the first user in the list (which should be the only user, if you followed the instructions)
 user = User.objects.all()[0]
 
+## Create a current account
 acc1 = Account.objects.create(acc_user=user, acc_type='CRT')
+## Prompt the account to create a UUID number
 acc1.uuid_generate()
 
+## Run the model method to open the account
+## The current account does not require a depost, so one is not specified
 acc1.open_account()
+
+## If run in sequence this will throw an AssertionError
 # acc1.withdraw(withdraw_value=101000)
+
+## Add a deposit
 acc1.deposit(dep_value=2000)
+
+## Withdraw
 acc1.withdraw(withdraw_value=101000)
 
-
+## Create a savingsaccount
 acc2 = Account.objects.create(acc_user=user, acc_type='SVG')
 acc2.uuid_generate()
 
+## This will throw an assertion error if run in sequence
 # acc.deposit(dep_value=50)
+
+## open account with starting balance
 acc2.open_account(dep_value=1000)
+## Make a deposit
 acc2.deposit(dep_value=5330)
 
+## Make withdrawals
 acc2.withdraw(withdraw_value=75)
 acc2.withdraw(withdraw_value=35)
--- click2sure_bank/main/models.py
+++ click2sure_bank/main/models.py
@@ -7,66 +7,74 @@ import uuid
 # Create your models here.
 
 ACC_TYPES = (
-    ('CRT', 'Current'),
-    ('SVG', 'Savings'),
+	('CRT', 'Current'),
+	('SVG', 'Savings'),
 )
 
 class Account(models.Model):
-    acc_user = models.ForeignKey(User, related_name='user_accounts', on_delete=models.CASCADE, null=True, blank=True)
-    acc_id = models.CharField(max_length=255, default='', blank=True, null=True)
-    acc_type = models.CharField(max_length=10, default='CRT', blank=True, null=True, choices=ACC_TYPES)
-    acc_balance = models.FloatField(null=True, blank=True, default=0)
-    acc_created = models.DateTimeField(auto_now_add=True)
-    acc_last_updated = models.DateTimeField(auto_now=True)
+	acc_user = models.ForeignKey(User, related_name='user_accounts', on_delete=models.CASCADE, null=True, blank=True)
+	acc_id = models.CharField(max_length=255, default='', blank=True, null=True)
+	acc_type = models.CharField(max_length=10, default='CRT', blank=True, null=True, choices=ACC_TYPES)
+	acc_balance = models.FloatField(null=True, blank=True, default=0)
+	acc_created = models.DateTimeField(auto_now_add=True)
+	acc_last_updated = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-    	return "%s) %s => R%s (\n%s)"%(self.pk, self.acc_user, self.acc_balance, self.acc_last_updated)
+	def __str__(self):
+		return "%s) %s => R%s (\n%s)"%(self.pk, self.acc_user, self.acc_balance, self.acc_last_updated)
 
-    def uuid_generate(self):
-        self.acc_uuid = "%s"%(str(uuid.uuid4()))
-        self.save()
-        print("Created UUID for %s"%self)
+	def uuid_generate(self):
+		"""
+		Generating a unique ID
+		"""
+		self.acc_uuid = "%s"%(str(uuid.uuid4()))
+		self.save()
+		print("Created UUID for %s"%self)
 
-    def open_account(self, dep_value=0):
-    	if self.acc_type == "SVG":
-    		assert dep_value >= 1000, "Initial Deposit value has to be ≥ R1,000."
-    		self.acc_balance = dep_value
-    		self.save()
-    		tr = Transaction.objects.create(tr_user=self.acc_user, tr_account=self, tr_type='DEP', tr_value=dep_value, tr_acc_balance_at=self.acc_balance)
-    		print(self.__str__, tr.__str__)
-    	if self.acc_type == "CRT":
-    		self.acc_balance = dep_value
-    		self.save()
-    		tr = Transaction.objects.create(tr_user=self.acc_user, tr_account=self, tr_type='DEP', tr_value=dep_value, tr_acc_balance_at=self.acc_balance)
-    		print(self.__str__, tr.__str__)
+	def open_account(self, dep_value=0):
+		"""
+		Lazy model method that accepts an account type. This should be a hard coded function for each account type.
+		"""
+		if self.acc_type == "SVG":
+			assert dep_value >= 1000, "Initial Deposit value has to be ≥ R1,000."
+			self.acc_balance = dep_value
+			self.save()
+			tr = Transaction.objects.create(tr_user=self.acc_user, tr_account=self, tr_type='DEP', tr_value=dep_value, tr_acc_balance_at=self.acc_balance)
+			print(self.__str__, tr.__str__)
+		if self.acc_type == "CRT":
+			self.acc_balance = dep_value
+			self.save()
+			tr = Transaction.objects.create(tr_user=self.acc_user, tr_account=self, tr_type='DEP', tr_value=dep_value, tr_acc_balance_at=self.acc_balance)
+			print(self.__str__, tr.__str__)
 
-    def deposit(self, dep_value):
-    	if self.acc_type == 'SVG':
-    		assert self.acc_balance >= 1000, "Balance needs to be R1,000 or more"
-    		self.acc_balance += dep_value
-    		self.save()
-    		tr = Transaction.objects.create(tr_user=self.acc_user, tr_account=self, tr_type='DEP', tr_value=dep_value, tr_acc_balance_at=self.acc_balance)
-    		print(self.__str__, tr.__str__)
-    	if self.acc_type == 'CRT':
-    		self.acc_balance += dep_value
-    		self.save()
-    		tr = Transaction.objects.create(tr_user=self.acc_user, tr_account=self, tr_type='DEP', tr_value=dep_value, tr_acc_balance_at=self.acc_balance)
-    		print(self.__str__, tr.__str__)
+	def deposit(self, dep_value):
+		if self.acc_type == 'SVG':
+			assert self.acc_balance >= 1000, "Balance needs to be R1,000 or more"
+			self.acc_balance += dep_value
+			self.save()
+			tr = Transaction.objects.create(tr_user=self.acc_user, tr_account=self, tr_type='DEP', tr_value=dep_value, tr_acc_balance_at=self.acc_balance)
+			print(self.__str__, tr.__str__)
+		if self.acc_type == 'CRT':
+			self.acc_balance += dep_value
+			self.save()
+			tr = Transaction.objects.create(tr_user=self.acc_user, tr_account=self, tr_type='DEP', tr_value=dep_value, tr_acc_balance_at=self.acc_balance)
+			print(self.__str__, tr.__str__)
 
-    def withdraw(self, withdraw_value):
-    	if self.acc_type == 'SVG':
-    		assert (self.acc_balance - withdraw_value) >= 1000, "Remaining balance after withdrawal needs to be ≥ R1,000. This transaction would result in a balance of R%s"%(self.acc_balance - withdraw_value)
-    		self.acc_balance -= withdraw_value
-    		self.save()
-    		tr = Transaction.objects.create(tr_user=self.acc_user, tr_account=self, tr_type='WTD', tr_value=withdraw_value, tr_acc_balance_at=self.acc_balance)
-    		print(self.__str__, tr.__str__)
-    	if self.acc_type == 'CRT':
-    		assert (self.acc_balance - withdraw_value) >= -100000, "Overdraft limit is = R100,000. This transaction would result in a balance of R%s"%(self.acc_balance - withdraw_value)
-    		self.acc_balance -= withdraw_value
-    		self.save()
-    		tr = Transaction.objects.create(tr_user=self.acc_user, tr_account=self, tr_type='WTD', tr_value=withdraw_value, tr_acc_balance_at=self.acc_balance)
+	def withdraw(self, withdraw_value):
+		if self.acc_type == 'SVG':
+			assert (self.acc_balance - withdraw_value) >= 1000, "Remaining balance after withdrawal needs to be ≥ R1,000. This transaction would result in a balance of R%s"%(self.acc_balance - withdraw_value)
+			self.acc_balance -= withdraw_value
+			self.save()
+			tr = Transaction.objects.create(tr_user=self.acc_user, tr_account=self, tr_type='WTD', tr_value=withdraw_value, tr_acc_balance_at=self.acc_balance)
+			print(self.__str__, tr.__str__)
+		if self.acc_type == 'CRT':
+			assert (self.acc_balance - withdraw_value) >= -100000, "Overdraft limit is = R100,000. This transaction would result in a balance of R%s"%(self.acc_balance - withdraw_value)
+			self.acc_balance -= withdraw_value
+			self.save()
+			tr = Transaction.objects.create(tr_user=self.acc_user, tr_account=self, tr_type='WTD', tr_value=withdraw_value, tr_acc_balance_at=self.acc_balance)
 
 
+## Transactions
+## The idea is to create a transaction with every deposit and withdrawal, doing so automatically as a model method above.
 class Transaction(models.Model):
 	tr_user = models.ForeignKey(User, related_name='user_transactions', on_delete=models.CASCADE, null=True, blank=True)
 	tr_account = models.ForeignKey(Account, related_name = 'account_transactions', on_delete=models.CASCADE, null=False, blank=False)
