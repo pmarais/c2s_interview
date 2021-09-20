a784b3e9918b5603b7d9e2e1002eaf517b93e59c
Philip Marais <philipmarais@gmail.com>
2021-09-20 11:59:24 +0200

    Final migration of the 1-2 accounts types

--- click2sure_bank/main/helpers.py
+++ click2sure_bank/main/helpers.py
@@ -6,10 +6,12 @@ user = User.objects.all()[0]
 
 ## Create a current account
 acc1 = CurrentAccount.objects.create(acc_user=user)
+## Create a unique ID for account
+acc1.uuid_generate()
 
 ## Run the model method to open the account
 ## The current account does not require a depost, so one is not specified
-acc1.open_account(dep_value=0)
+acc1.open_account(dep_value=0, acc_overdraft_limit=100000)
 
 ## If run in sequence this will throw an AssertionError
 acc1.withdraw(withdraw_value=101000)
@@ -20,8 +22,19 @@ acc1.deposit(dep_value=2000)
 ## Withdraw
 acc1.withdraw(withdraw_value=101000)
 
+
+#############
+## Savings ##
+#############
+## Import the required models iobjects
+from main.models import *
+
+## Select the first user in the list (which should be the only user, if you followed the instructions)
+user = User.objects.all()[0]
+
 ## Create a savingsaccount
 acc2 = SavingsAccount.objects.create(acc_user=user)
+## Create a unique ID for account
 acc2.uuid_generate()
 
 ## This will throw an assertion error if run in sequence
--- click2sure_bank/main/models.py
+++ click2sure_bank/main/models.py
@@ -11,6 +11,7 @@ class CurrentAccount(models.Model):
 	acc_id = models.CharField(max_length=255, default='', blank=True, null=True)
 	acc_balance = models.FloatField(null=True, blank=True, default=0)
 	acc_created = models.DateTimeField(auto_now_add=True)
+	acc_overdraft_limit = models.FloatField(null=True, blank=True, default=0)
 	acc_last_updated = models.DateTimeField(auto_now=True)
 
 	def __str__(self):
@@ -24,11 +25,13 @@ class CurrentAccount(models.Model):
 		self.save()
 		print("Created UUID for %s"%self)
 
-	def open_account(self, dep_value=0):
-		self.acc_balance = dep_value
-		self.save()
-		tr = Transaction.objects.create(tr_user=self.acc_user, tr_caccount=self, tr_type='DEP', tr_value=dep_value, tr_acc_balance_at=self.acc_balance)
-		print(self.__str__, tr.__str__)
+	def open_account(self, dep_value=0, acc_overdraft_limit=0):
+		if acc_overdraft_limit <= 100000:
+			self.acc_balance = dep_value
+			self.acc_overdraft_limit = acc_overdraft_limit
+			self.save()
+			tr = Transaction.objects.create(tr_user=self.acc_user, tr_caccount=self, tr_type='DEP', tr_value=dep_value, tr_acc_balance_at=self.acc_balance)
+			print(self.__str__, tr.__str__)
 
 	def deposit(self, dep_value):
 		self.acc_balance += dep_value
@@ -37,10 +40,11 @@ class CurrentAccount(models.Model):
 		print(self.__str__, tr.__str__)
 
 	def withdraw(self, withdraw_value):
-		assert (self.acc_balance - withdraw_value) >= -100000, "Overdraft limit is = R100,000. This transaction would result in a balance of R%s"%(self.acc_balance - withdraw_value)
+		assert (self.acc_balance - withdraw_value) >= -self.acc_overdraft_limit, "Overdraft limit is = %s. This transaction would result in a balance of R%s"%(self.acc_overdraft_limit, self.acc_balance - withdraw_value)
 		self.acc_balance -= withdraw_value
 		self.save()
 		tr = Transaction.objects.create(tr_user=self.acc_user, tr_caccount=self, tr_type='WTD', tr_value=withdraw_value, tr_acc_balance_at=self.acc_balance)
+		print(tr)
 
 class SavingsAccount(models.Model):
 	acc_user = models.ForeignKey(User, related_name='user_savings_accounts', on_delete=models.CASCADE, null=True, blank=True)
@@ -68,7 +72,7 @@ class SavingsAccount(models.Model):
 		print(self.__str__, tr.__str__)
 
 	def deposit(self, dep_value):
-		assert self.acc_balance >= 1000, "Balance needs to be R1,000 or more"
+		assert self.acc_balance >= 1000, "Balance needs to be R1,000 or more at all times"
 		self.acc_balance += dep_value
 		self.save()
 		tr = Transaction.objects.create(tr_user=self.acc_user, tr_saccount=self, tr_type='DEP', tr_value=dep_value, tr_acc_balance_at=self.acc_balance)
